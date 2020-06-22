7fbf35eb28f6cb59898b2006f7d481d08a075458
Mahmudul Sajib <32324883+mahmud-sajib@users.noreply.github.com>
2020-06-22 10:41:28 +0600

    Update settings.py

--- CommunityApp/settings.py
+++ CommunityApp/settings.py
@@ -79,6 +79,7 @@ WSGI_APPLICATION = 'CommunityApp.wsgi.application'
 
 DATABASES = {
     'default': {
+        
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #         'ENGINE': 'django.db.backends.sqlite3',
