d865992c7d66c04938c5a3cd546f871dc1fbbbdd
mahmud-sajib <shout.mahmud@gmail.com>
2020-06-23 20:02:57 +0600

    files added

--- registration/signals.py
+++ registration/signals.py
@@ -1,17 +1,19 @@
-from forum.models import Author
-from django.db.models.signals import post_save
-from django.contrib.auth.models import User
-from django.dispatch import receiver
+# from forum.models import Author
+# from django.db.models.signals import post_save
+# from django.contrib.auth.models import User
+# from django.dispatch import receiver
 
 # Create profile signal
-@receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        print("Profile Created")
+# @receiver(post_save, sender=User)
+# def create_profile(sender, instance, created, **kwargs):
+#     if created:
+#         print("Profile Created")
 
 # Save profile signal
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, created, **kwargs):
-    if created == False:
-        instance.author.save()
-        print("Profile Updated")
+# @receiver(post_save, sender=User)
+# def save_profile(sender, instance, created, **kwargs):
+#     if created == True:
+#         pass
+#     else:
+#         instance.author.save()
+#         print("Profile Updated")
