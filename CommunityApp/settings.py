dfb7461dc954381f8199d4b0419973af5f575c64
mahmud-sajib <shout.mahmud@gmail.com>
2020-06-24 22:10:15 +0600

    files updated

--- CommunityApp/settings.py
+++ CommunityApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -81,9 +82,9 @@ DATABASES = {
     'default': {
         'NAME': 'CommunityAppDB',
         'ENGINE': 'django.db.backends.postgresql',
-        'USER': 'postgres',
-        'PASSWORD': 'admin123',
-        'HOST': 'localhost',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'admin123',
+        # 'HOST': 'localhost',
     }
 }
 
@@ -182,3 +183,6 @@ BATON = {
         },
     )
 }
+
+
+django_heroku.settings(locals())
