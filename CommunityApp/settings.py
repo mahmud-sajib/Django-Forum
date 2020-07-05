7b2c3a137edf4c842da0584559c0969b55383303
Mahmudul Sajib <32324883+mahmud-sajib@users.noreply.github.com>
2020-07-05 10:37:41 +0600

    Update settings.py

--- CommunityApp/settings.py
+++ CommunityApp/settings.py
@@ -140,8 +140,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sajibforest@gmail.com'
-EMAIL_HOST_PASSWORD = 'wcxelosgnxuxmqiq'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = 'MedicAid Team <noreply@medicaid.org>'
 
 django_heroku.settings(locals())
